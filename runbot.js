7ffbda15a072e568b64a7070ddf8490d7cfe0344
snt1986 <s.lopez_@outlook.com>
2022-06-18 11:36:20 +0200

    change strategy

--- runbot.js
+++ runbot.js
@@ -3,7 +3,7 @@ const Bot = require("./src/Bot/Bot");
 const {
     isUpperSellFunction,
     priceToRebuyFunction
-} = require("./src/ActionsFunctions/real/InitialStrategy");
+} = require("./src/ActionsFunctions/real/07-51");
 
 const bot = new Bot({
     "modulesFunctions": {
