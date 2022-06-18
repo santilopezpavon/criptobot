9f2de8661a7996069e90282de847748d5893c863
snt1986 <s.lopez_@outlook.com>
2022-06-18 14:52:13 +0200

    change bot

--- runbot.js
+++ runbot.js
@@ -3,7 +3,7 @@ const Bot = require("./src/Bot/Bot");
 const {
     isUpperSellFunction,
     priceToRebuyFunction
-} = require("./src/ActionsFunctions/real/07-51");
+} = require("./src/ActionsFunctions/real/safe");
 
 const bot = new Bot({
     "modulesFunctions": {
